760c18889624a72560453beb97a43f23981c42d6
Bozena <crazyhappygame@gmail.com>
2018-08-19 17:09:28 +0200

    Add zlib

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,6 +1,8 @@
 #include "engine.h"
 #include "Box2D/Box2D.h"
 #include "recast/Detour/DetourNode.h"
+#include "zlib.h"
+
 #include <iostream>
 #include <sstream>
 
@@ -8,12 +10,22 @@ namespace engine {
 std::string getInfo()
 {
     std::ostringstream o;
-    o << "engine info\n";
+    o << "External libs info\n";
     o << "Box2D version: " << b2_version.major << "." << b2_version.minor
       << "." << b2_version.revision << "\n";
 
     dtNodeQueue q(10);
     o << "Recast dtNodeQueue: " << q.empty() << "\n";
+    o << "zlib version: " << zlibVersion() << "\n";
     return o.str();
+}
+
+std::string getInfoExpected()
+{
+    return R"(External libs info
+Box2D version: 2.3.2
+Recast dtNodeQueue: 1
+zlib version: 1.2.8
+)";
 }
 } // namespace engine
--- tests/game1/main.cpp
+++ tests/game1/main.cpp
@@ -5,5 +5,10 @@ int main()
 {
     std::cout << "game1\n";
     std::cout << engine::getInfo();
+    if (engine::getInfoExpected() != engine::getInfo())
+    {
+        std::cout << engine::getInfoExpected() << "!=" << engine::getInfo();
+        return 1;
+    }
     return 0;
 }
