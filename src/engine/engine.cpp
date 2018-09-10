5b171176f019281b12fc75f11d286190cff67835
Bozena <crazyhappygame@gmail.com>
2018-09-10 20:16:09 +0200

    Add chipmunk

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,5 +1,6 @@
 #include "engine.h"
 #include "Box2D/Box2D.h"
+#include "chipmunk/chipmunk.h"
 #include "recast/Detour/DetourNode.h"
 #include "zlib.h"
 
@@ -10,10 +11,10 @@ namespace engine {
 std::string getInfo()
 {
     std::ostringstream o;
-    o << "External libs info\n";
+    o << "External libs info:\n";
     o << "Box2D version: " << b2_version.major << "." << b2_version.minor
       << "." << b2_version.revision << "\n";
-
+    o << "chipmunk: " << cpVersionString << "\n";
     dtNodeQueue q(10);
     o << "Recast dtNodeQueue: " << q.empty() << "\n";
     o << "zlib version: " << zlibVersion() << "\n";
@@ -22,8 +23,9 @@ std::string getInfo()
 
 std::string getInfoExpected()
 {
-    return R"(External libs info
+    return R"(External libs info:
 Box2D version: 2.3.2
+chipmunk: 7.0.1
 Recast dtNodeQueue: 1
 zlib version: 1.2.8
 )";
