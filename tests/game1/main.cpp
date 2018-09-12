6b3553dbbd514ab85adf78eef58b205e01a2ba9d
Bozena <crazyhappygame@gmail.com>
2018-09-12 18:46:53 +0200

    Add uv, fix tests

--- tests/game1/main.cpp
+++ tests/game1/main.cpp
@@ -6,7 +6,7 @@ int main()
     std::cout << "game1\n";
     std::cout << engine::getInfoActual();
 
-    if (engine::getInfoActual().compare(engine::getInfoActual()) != 0)
+    if (engine::getInfoExpected().compare(engine::getInfoActual()) != 0)
     {
         std::cout << engine::getInfoExpected() << "!=\n" << engine::getInfoActual();
         return 1;
