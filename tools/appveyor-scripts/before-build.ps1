eade3e6207d15c0ecefde7b15ba2397a96f02bfb
CHP <crazyhappygame@gmail.com>
2018-12-10 01:47:24 +0100

    Windows use ninja (#35)
    * Windows use ninja
    
    * Fix VsDevCmd.bat
    
    * vcvarsall.bat
    
    * NUL
    
    * Add x64, arm, arm64
    
    * Disable arm

--- tools/appveyor-scripts/before-build.ps1
+++ tools/appveyor-scripts/before-build.ps1
@@ -1,17 +1,12 @@
 Set-PSDebug -Trace 1
 $python = "C:\\Python27\\python.exe"
 
-function Update-SubModule
-{
-    Push-Location $env:APPVEYOR_BUILD_FOLDER
-    & git submodule init
-    & git submodule update --recursive --depth=1
-    Pop-Location
-}
+& choco install ninja
+if ($lastexitcode -ne 0) {throw}
+& ninja --version
+if ($lastexitcode -ne 0) {throw}
 
 if ($env:build_type -like "android_lib") {
-    & choco install ninja
-    & ninja --version
     & $python -u .\tools\appveyor-scripts\setup_android.py --ndk_only
     if ($lastexitcode -ne 0) {throw}
 }
